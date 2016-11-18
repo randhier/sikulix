89dbfa7a47a6dd2f599c67312a05a7d55c0c7755
Randhier <randhier@gmail.com>
2016-11-18 17:39:25 -0500

    quick and dirty OS independent launcher

--- src/test/java/com/spotify/app/SpotifyTest.java
+++ src/test/java/com/spotify/app/SpotifyTest.java
@@ -16,11 +16,23 @@ public class SpotifyTest {
     @BeforeClass
     public void setUp() {
         System.out.println("Setup");
-        spotify = App.open("/Applications/Spotify.app/Contents/MacOS/Spotify");
+        spotify = App.open(getLaunch());
     }
 
     @AfterClass
     public void tearDown() {
         System.out.println("Teardown");
+        spotify.close();
+    }
+
+    public static String getLaunch() {
+        String OS = System.getProperty("os.name").toLowerCase();
+        if (OS.indexOf("mac") >= 0) {
+            OS = "/Applications/Spotify.app/Contents/MacOS/Spotify";
+        } else {
+            // assuming if not MAC then Win. Enter win launcher location here
+            OS = "spotify.exe";
+        }
+        return OS;
     }
 }
