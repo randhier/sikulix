6fd30b5fa3ad0f64a54503511ca07defa917683b
Randhier <randhier@gmail.com>
2016-11-25 01:10:19 -0500

    enter login info

--- src/test/java/com/spotify/app/SpotifyTest.java
+++ src/test/java/com/spotify/app/SpotifyTest.java
@@ -1,29 +1,36 @@
 package com.spotify.app;
 
+import org.sikuli.script.Key;
+import org.sikuli.script.KeyModifier;
 import org.testng.Assert;
 import org.testng.annotations.*;
 import org.sikuli.script.App;
-import org.sikuli.basics.Settings;
+import org.sikuli.script.Screen;
 import helpers.*;
 
 public class SpotifyTest {
 
-    App spotify;
+    private App spotify;
+    private Screen s;
 
     @Test
     public void main() {
-        System.out.println("Test");
+        s.type("a", KeyModifier.CMD);
+        s.type(Key.BACKSPACE);
+        s.type(System.getenv("SPOTIFY_USER"));
+        s.type(Key.TAB);
+        s.type(System.getenv("SPOTIFY_PASSWORD"));
     }
 
     @BeforeClass
     public void setUp() {
-        System.out.println("Setup");
+        s = new Screen();
         spotify = App.open(general.appLocation());
+        spotify.focus();
     }
 
     @AfterClass
     public void tearDown() {
-        System.out.println("Teardown");
         spotify.close();
     }
 }
